3135f8ec0ad2a61c78d91be0d287d8b3eac0e91b
VenDiago <alizg@code.edu.az>
2022-02-28 08:41:10 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace HomeTask002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student ven = new Student("Alik", "Gafarov", "P127", 20);
+            string nam;
+            string sur;
+            string grp;
+            do
+            {
+
+                Console.WriteLine("Name Daxil edin:");
+                nam = Console.ReadLine();
+
+            } while (!Student.CheckName(nam));
+            do
+            {
+
+                Console.WriteLine("Surname Daxil edin:");
+                sur = Console.ReadLine();
+
+            } while (!Student.CheckSurname(sur));
+            do
+            {
+
+                Console.WriteLine("GroupNo Daxil edin:");
+                grp = Console.ReadLine();
+
+            } while (!Student.CheckGroupno(grp));
+
+            if (!Student.CheckName(nam) == false && !Student.CheckSurname(sur) == false && !Student.CheckGroupno(grp) == false)
+            {
+
+                Console.WriteLine(ven.Getinfo());
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
+ 
---
+++ Student.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeTask002
+{
+    partial class Student
+    {
+        //Fields
+        public string Name;
+        public string Surname;
+        public string GroupNo;
+        public int Age;
+
+
+    }
+
+    partial class Student
+    {
+        public Student(string name, string surname, string groupno, int age)
+        {
+            //Constructor
+            Name = name;
+            Surname = surname;
+            GroupNo = groupno;
+            Age = age;
+        }
+    }
+    partial class Student
+    {
+        //Methods
+        public string Getinfo()
+        {
+            return $"\nName: {Name} \nSurname: {Surname} \nGroupNo: {GroupNo} \nAge: {Age}";
+
+        }
+        public static bool CheckName(string name)
+        {
+            if (name.Length >= 3)
+            {
+                bool IsUpper = false;
+
+
+                if (char.IsUpper(name[0]))
+                {
+                    IsUpper = true;
+
+                }
+
+                bool result = IsUpper;
+                return result;
+            }
+            return false;
+        }
+        public static bool CheckSurname(string surname)
+        {
+            if (surname.Length >= 3)
+            {
+                bool IsUpper = false;
+
+
+                if (char.IsUpper(surname[0]))
+                {
+                    IsUpper = true;
+
+                }
+
+                bool result = IsUpper;
+                return result;
+            }
+            return false;
+        }
+        public static bool CheckGroupno(string groupno)
+        {
+            if (groupno.Length == 4)
+            {
+                bool IsUpper = false;
+
+
+                if (char.IsUpper(groupno[0]))
+                {
+                    IsUpper = true;
+
+                }
+
+                bool result = IsUpper;
+                return result;
+            }
+            return false;
+        }
+    }
+}
+
